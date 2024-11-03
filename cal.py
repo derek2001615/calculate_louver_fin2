19e484ee69a19145d5805665759cac82aaa9e639
derek <derek2001615@gmail.com>
2024-11-04 05:31:34 +0800

    0

--- cal.py
+++ cal.py
@@ -1,31 +1,32 @@
 class MultiLouvered:
-    def __init__(self, V_c, L_p, viscosity_o, L_alpha, F_p, H, F_d, L_l, T_p, delta_f, Pr_o, A_o, A_c, density_m, Cp_o, T_o2, T_o1):
-        self.V_c = V_c
-        self.L_p = L_p
-        self.viscosity_o = viscosity_o
-        self.L_alpha = L_alpha
-        self.F_p = F_p
-        self.H = H
-        self.F_d = F_d
-        self.L_l = L_l
-        self.T_p = T_p
-        self.delta_f = delta_f
-        self.Pr_o = Pr_o
-        self.A_o = A_o
-        self.A_c = A_c
-        self.density_m = density_m
-        self.Cp_o = Cp_o
-        self.T_o2 = T_o2
-        self.T_o1 = T_o1
-        self.cal_Re_Lp()
-        self.cal_colburn_j_factor()
-        self.cal_fanning_friction_factor()
-        self.cal_h_o()
+    def __init__(self, **kwargs):
+        self.V_c = kwargs.get('V_c', 3.27)
+        self.L_p = kwargs.get('L_p', 0.0017)
+        self.viscosity_o = kwargs.get('viscosity_o', 1.85 * 10**-5)
+        self.L_alpha = kwargs.get('L_alpha', 23)
+        self.F_p = kwargs.get('F_p', 0.0014)
+        self.H = kwargs.get('H', 0.00815)
+        self.F_d = kwargs.get('F_d', 0.02)
+        self.L_l = kwargs.get('L_l', 0.0064)
+        self.T_p = kwargs.get('T_p', 0.01015)
+        self.delta_f = kwargs.get('delta_f', 0.0001)
+        self.Pr_o = kwargs.get('Pr_o', 0.71)
+        self.A_o = kwargs.get('A_o', 0.1785)
+        self.A_c = kwargs.get('A_c', 0.0084)
+        self.density_m = kwargs.get('density_m', 1.225)
+        self.Cp_o = kwargs.get('Cp_o', 1007)
+        self.T_o2 = kwargs.get('T_o2', 30)
+        self.T_o1 = kwargs.get('T_o1', 21)
 
-    def cal_Re_Lp(self):
+        self._calc_Re_Lp()
+        self._calc_colburn_j_factor()
+        self._calc_fanning_friction_factor()
+        self._calc_h_o()
+ 
+    def _calc_Re_Lp(self):
         self.Re_Lp = self.V_c * self.L_p / self.viscosity_o
         
-    def cal_colburn_j_factor(self):
+    def _calc_colburn_j_factor(self):
         self.j = self.Re_Lp**-0.487 * \
         (self.L_alpha / 90)**0.257 * \
         (self.F_p / self.L_p)**-0.13 * \
@@ -35,7 +36,7 @@ class MultiLouvered:
         (self.T_p / self.L_p)**-0.279 * \
         (self.delta_f / self.L_p)**-0.05
         
-    def cal_fanning_friction_factor(self):
+    def _calc_fanning_friction_factor(self):
         self.f = self.Re_Lp**-0.781 * \
         (self.L_alpha / 90)**0.444 * \
         (self.F_p / self.L_p)**-1.682 * \
@@ -51,7 +52,7 @@ class MultiLouvered:
         delta_P = self.f * self.A_o / self.A_c * self.density_m * self.V_c**2 / 2
         return delta_P
 
-    def cal_h_o(self):
+    def _calc_h_o(self):
         self.h_o = self.j * self.density_m * self.V_c * self.Cp_o / self.Pr_o**(2/3)
         
     def cal_heat_transfer(self):
--- test_cal.py
+++ test_cal.py
@@ -1,29 +1,32 @@
 from cal import MultiLouvered as cal
 
-def set_value():
-    return (
-        3.27,                         # V_c
-        0.0017,                       # L_p
-        1.85 * 10**-5,                # viscosity_o
-        23,                           # L_alpha
-        0.0014,                       # F_p
-        0.00815,                      # H
-        0.02,                         # F_d
-        0.0064,                       # L_l
-        0.01015,                      # T_p
-        0.0001,                       # delta_f
-        0.71,                         # Pr_o
-        0.1785,                       # A_o
-        0.0084,                       # A_c
-        1.225,                        # density_m
-        1007,                         # Cp_o
-        30,                           # T_o2
-        21                            # T_o1
-    )
+def get_data001():
+    return {
+        'V_c': 3.27,
+        'L_p': 0.0017,
+        'viscosity_o': 1.85 * 10**-5,
+        'L_alpha': 23,
+        'F_p': 0.0014,
+        'H': 0.00815,
+        'F_d': 0.02,
+        'L_l': 0.0064,
+        'T_p': 0.01015,
+        'delta_f': 0.0001,
+        'Pr_o': 0.71,
+        'A_o': 0.1785,
+        'A_c': 0.0084,
+        'density_m': 1.225,
+        'Cp_o': 1007,
+        'T_o2': 30,
+        'T_o1': 21
+    }
+
+def get_data002():
+    return {}
 
 def main():
-    data001 = set_value()
-    caldata001 = cal(*data001)
+    data001 = get_data001()
+    caldata001 = cal(**data001)
 
     print("Re_Lp:", caldata001.Re_Lp)
     print("Colburn j-factor:", caldata001.j)
@@ -32,6 +35,19 @@ def main():
     print("Pressure drop:", caldata001.cal_pressure_drop())
     print("Heat transfer coefficient h_o:", caldata001.h_o)
     print("Heat transfer Q:", caldata001.cal_heat_transfer())
+
+
+    data002 = get_data002()
+    caldata002 = cal(**data002)
+
+    print("Re_Lp:", caldata002.Re_Lp)
+    print("Colburn j-factor:", caldata002.j)
+    print("Fanning friction factor:", caldata002.f)
+    print("Nusselt number:", caldata002.cal_Nusselt_number())
+    print("Pressure drop:", caldata002.cal_pressure_drop())
+    print("Heat transfer coefficient h_o:", caldata002.h_o)
+    print("Heat transfer Q:", caldata002.cal_heat_transfer())
+
 
 if __name__ == '__main__':
     main()
